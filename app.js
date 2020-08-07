ef24a85eed1997745eb27d81737e307cff6d3ae9
Hp Pavillion@Wavinya MINGW64 ~/Desktop$$ git committry thi sout <estherwavinya80@gmail.com>
2020-08-07 10:32:57 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// Using a for loop
+
+var looping = function(n) {
+    var a = 0, b = 1, f = 1;
+    for(var i = 2; i <= n; i++) {
+        f = a + b;
+        a = b;
+        b = f;
+    }
+    
+    return f;
+};
+
+
+// Using a while loop
+
+function fibonacci(num){
+    var a = 1, b = 0, temp;
+
+    while (num >= 0){
+        temp = a;
+        a = a + b;
+        b = temp;
+    }
+
+    return b;
+}
